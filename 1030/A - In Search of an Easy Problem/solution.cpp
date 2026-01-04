8548e89c5484044d1411abc16fc30cdf8453fd88
Fazal Qadir <fazalqadir939@gmail.com>
2026-01-04 18:36:24 +0500

    Add In Search of an Easy Problem [A] from Codeforces

---
+++ 1030/A - In Search of an Easy Problem/solution.cpp
@@ -0,0 +1,1 @@
+//// Created by User on 04-Jan-26.//#include <iostream>using namespace std;int main(){    int n;    cin >> n;    int arr[n];    bool g = false;    for (int i =0;i<n;i++)    {        cin >>arr[i];        if (arr[i]==1)        {            g = true;            break;        }    }    if (g==true)    {        cout << "HARD";    }    else    {        cout << "EASY";    } }
