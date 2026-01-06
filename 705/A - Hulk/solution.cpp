f06adb919bcf277f3ef682989c3f1616bbd6c495
Fazal Qadir <fazalqadir939@gmail.com>
2026-01-06 20:15:46 +0500

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.cpp
@@ -0,0 +1,1 @@
+//// Created by Fazal Qadir on 06-Jan-26.//#include <iostream>using namespace std;int main(){    int n;    cin>>n;    for (int i = 1;i<=n;i++)    {        if (i%2==0)        {            cout << "I love ";        }        else        {            cout << "I hate ";        }        if (i+1<=n)        {            cout << "that ";        }    }    cout << "it";}
