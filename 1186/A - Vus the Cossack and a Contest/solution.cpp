5a24f6c66bbef36b6361c1761f10d6a28e33e33c
Fazal Qadir <fazalqadir939@gmail.com>
2026-02-07 03:51:48 +0500

    Add Vus the Cossack and a Contest [A] from Codeforces

--- 1186/A - Vus the Cossack and a Contest/solution.cpp
+++ 1186/A - Vus the Cossack and a Contest/solution.cpp
@@ -1,1 +1,1 @@
-#include<iostream>using namespace std;int main(){    int n,m,k;    cin >> n>>m>>k;    if(m>=n&&k>=n){        cout << "YES";    }    else{        cout << "NO";    }}
+#include<iostream>using namespace std;int main(){    int ni,m,k;    cin >> ni>>m>>k;    if(m>=ni&&k>=ni){        cout << "YES";    }    else{        cout << "NO";    }}
