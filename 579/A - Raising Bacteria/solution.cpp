6655ad14b5ec098e1c103cbb16c079a467479a8a
Fazal Qadir <fazalqadir939@gmail.com>
2026-06-10 23:43:24 +0500

    Add Raising Bacteria [A] from Codeforces

--- 579/A - Raising Bacteria/solution.cpp
+++ 579/A - Raising Bacteria/solution.cpp
@@ -1,12 +1,16 @@
-#include <bits/stdc++.h>
+//
+// Created by fazalqadir on 06/10/26.
+//
+#include <iostream>
+#include <bit>
 using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
+    unsigned int n;
     cin >> n;
-    cout << __builtin_popcount(n) << "
+    cout << std::popcount(n) << "
 ";
     return 0;
 }
