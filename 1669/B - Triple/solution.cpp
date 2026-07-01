8cdd784a74039fce84d2736ec6d01ad6b7ef538b
Fazal Qadir <fazalqadir939@gmail.com>
2026-07-01 09:09:34 +0500

    Add Triple [B] from Codeforces

---
+++ 1669/B - Triple/solution.cpp
@@ -0,0 +1,93 @@
+// =======================================================================================
+//
+//                         بِسْمِ ٱللّٰهِ ٱلرَّحْمٰنِ ٱلرَّحِيمِ
+// "وَلَقَدْ خَلَقْنَا الْإِنْسَانَ وَنَعْلَمُ مَا تُوَسْوِسُ بِهِ نَفْسُهُ ۖ وَنَحْنُ أَقْرَبُ إِلَيْهِ مِنْ حَبْلِ الْوَرِيدِ" (Quran 50:16)
+//
+// Monday , 22 June,2K26                                    Fazal Qadir
+//      codeforces Current Rating: 1237  --> Pupil   -->   @fazalqadir
+// =======================================================================================
+// Created by Fazal Qadir on 6/22/2026.
+ 
+#include<bits/stdc++.h>
+using namespace std;
+// --- Shortened ---
+ 
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+ 
+// --- Macros ---
+ 
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define fi first
+#define se second
+ 
+// --- Local Debugger ---
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#else
+#define debug(x)
+#endif
+ 
+// --- User Defined function Part ---
+ 
+void solve();
+ 
+ 
+// --- main function ---
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    // ---  Don't change above ---
+    int test=1;
+    cin >> test;
+    while(test--){
+        solve();
+    }
+}
+ 
+// ---  Actual code itthe rakh ---
+ 
+void solve(){
+    int n;
+    cin >> n;
+    vi vec(n);
+    for(int i=0;i<n;i++) {
+        cin >> vec[i];
+    }
+    sort(all(vec));
+    int value,count=1,max_count=0;
+    for(int i=1;i<n;i++) {
+        if (vec[i]==vec[i-1]) {
+            count++;
+        }
+        else {
+            count=1;
+        }
+        if (count>max_count) {
+            max_count=count;
+            value=vec[i];
+            if (max_count==3) {
+                break;
+            }
+        }
+    }
+    if (max_count>=3) {
+        cout << value << '
+';
+    }
+    else {
+        cout << -1 << '
+';
+    }
+}
+ 
+// This Template can only be use at AIK university. اسے کسی اور پلیٹ فارم پر استعمال کرنے کے لیے اجازت حاصل کریں۔
