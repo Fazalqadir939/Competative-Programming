edc89949112f20c4a2639d93c504b7a638819d28
Fazal Qadir <fazalqadir939@gmail.com>
2026-08-05 21:23:30 +0500

    Add Two Permutations [A] from Codeforces

---
+++ 1761/A - Two Permutations/solution.cpp
@@ -0,0 +1,32 @@
+// =======================================================================================
+//      August,2K26                                          Fazal Qadir
+//      codeforces                                         -->   @fazalqadir
+// =======================================================================================
+ 
+ 
+#include <bits/stdc++.h>
+using namespace std;
+void mantri();
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int t=1;
+    cin >> t;
+    while (t--)
+    {
+       mantri();
+       cout << '
+';
+    }
+    return 0;
+}
+ 
+void mantri(){
+    int n, a, b;
+    cin >> n >> a >> b;
+    if ((a == n&&b == n) || (a +b<=n - 2))
+        cout << "Yes";
+    else
+        cout << "No";
+    }
